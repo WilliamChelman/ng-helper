2dc58e8b700fb1e8845803e8e077f4e6b98adb16
William Chelman <william.chelman@gmail.com>
2018-05-11 11:16:10 +0200

    Better nodemon watching

--- src/bin/serve.ts
+++ src/bin/serve.ts
@@ -1,7 +1,8 @@
 import path from 'path';
+import { Observable, Subject } from 'rxjs';
 
-import { exec } from '../exec';
-import { getProjects, ProjectType } from '../projects-fetch';
+import { execO } from '../exec';
+import { getProjects, IProject, ProjectType } from '../projects-fetch';
 
 export async function serve(projectNames: string[], options: IServeOptions) {
     const allProjects = await getProjects(options.projectRoot);
@@ -20,15 +21,40 @@ export async function serve(projectNames: string[], options: IServeOptions) {
         const project = allProjects.projects[name];
         switch (project.projectType) {
             case ProjectType.APP:
-                return exec('ng', ['serve', name], { cwd: options.projectRoot }).catch(err => console.error(err));
+                return serveApp(name, project, options);
             case ProjectType.LIB:
-                const src = path.join(options.projectRoot, project.sourceRoot);
-                const args = [`--watch ${src}`, '--ext ts,html,css,scss', `--exec 'ng build ${name}'`];
-                return exec('nodemon', args, { cwd: options.projectRoot, shell: true, stdio: ['pipe', process.stdout, process.stderr] });
+                return serveLib(name, project, options);
             default:
                 throw new Error(`Unknown project type '${project.projectType}'`);
         }
     });
+}
+
+function serveApp(name: string, app: IProject, options: IServeOptions): Observable<void> {
+    const subject = new Subject<void>();
+
+    execO('ng', ['serve', name], { cwd: options.projectRoot }).subscribe(message => {
+        console.log('NEXT', message);
+    }, console.error);
+
+    return subject;
+}
+
+function serveLib(name: string, library: IProject, options: IServeOptions): Observable<void> {
+    const subject = new Subject<void>();
+    const src = path.join(options.projectRoot, library.sourceRoot);
+    const args = [`--watch ${src}`, '--ext ts,html,css,scss', `--exec 'ng build ${name}'`];
+    execO('nodemon', args, {
+        cwd: options.projectRoot,
+        shell: true
+    }).subscribe(message => {
+        console.log(message);
+        if (message.includes('[nodemon] clean exit')) {
+            subject.next();
+        }
+    }, console.error);
+
+    return subject;
 }
 
 export interface IServeOptions {
--- src/exec.ts
+++ src/exec.ts
@@ -1,23 +1,32 @@
 import { spawn, SpawnOptions } from 'child_process';
+import { Observable, Subject } from 'rxjs';
 
-export function exec(command: string, args?: string[], options?: SpawnOptions): Promise<any> {
-    return new Promise((resolve, reject) => {
-        const childProcess = spawn(command, args, options);
+export function execO(command: string, args?: string[], spawnOptions?: SpawnOptions): Observable<string> {
+    const subject = new Subject<string>();
 
-        let killed = false;
+    const child = spawn(command, args, spawnOptions);
+    let killed = false;
 
-        const killer = () => {
-            childProcess.kill();
-            killed = true;
-        };
+    const killer = () => {
+        child.kill();
+        killed = true;
+    };
 
-        process.on('SIGINT', killer);
+    process.on('SIGINT', killer);
 
-        childProcess.on('exit', code => {
-            process.removeListener('SIGINT', killer);
-            if (!killed) {
-                code === 0 ? resolve() : reject(code);
+    child.stdout!.on('data', data => subject.next(data.toString()));
+
+    child.stderr!.on('data', data => subject.error(data.toString()));
+
+    child.on('exit', code => {
+        process.removeListener('SIGINT', killer);
+        if (!killed) {
+            if (code !== 0) {
+                subject.error(code);
             }
-        });
+            subject.complete();
+        }
     });
+
+    return subject;
 }
